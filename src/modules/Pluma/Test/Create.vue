afeefca8cb767f4754ec62e06495dd24356875d5
Princess <princessellen0016@yahoo.com>
2018-08-14 13:49:31 +0800

    .

--- src/modules/Pluma/Test/Create.vue
+++ src/modules/Pluma/Test/Create.vue
@@ -3,58 +3,13 @@
     <v-container fluid grid-list-lg>
       <v-layout row wrap>
         <v-flex md6 xs12>
-          <current-course-progress class="mb-3"></current-course-progress>
+          <!-- <current-course-progress class="mb-3"></current-course-progress> -->
 
-          <to-do class="mb-3"></to-do>
+          <!-- <to-do class="mb-3"></to-do> -->
 
-          <my-students class="mb-3"></my-students>
+          <!-- <my-students class="mb-3"></my-students> -->
 
           <my-most-popular-courses></my-most-popular-courses>
-         <!--  <v-card hover>
-            <carousel
-              :autoplayTimeout="1000"
-              :paginationActiveColor="app.dark ? '#38a3ee' : '#0c5689'"
-              paginationColor="grey"
-              :perPage="1"
-              autoplay
-              >
-              <template
-                v-for="(item, i) in carousel.items"
-                >
-                <slide :key="i">
-                  <v-card flat :href="item.link">
-                    <v-card-media
-                      height="160"
-                      :src="item.thumbnail"
-                      >
-                    </v-card-media>
-                    <v-card-text>
-                      <p
-                        class="body-2 mb-2 primary--text text--lighten-2">
-                        <strong v-html="trans(item.category)"></strong>
-                      </p>
-                      <v-tooltip bottom>
-                        <h3
-                          slot="activator"
-                          class="title mb-3 title__text--ellipsis"
-                          v-html="trans(item.title)"
-                          >
-                        </h3>
-                        <span v-html="trans(item.title)"></span>
-                      </v-tooltip>
-                      <p>
-                        <span
-                          class="text--ellipsis"
-                          v-html="trans(item.description)"
-                          >
-                        </span>
-                      </p>
-                    </v-card-text>
-                  </v-card>
-                </slide>
-              </template>
-            </carousel>
-          </v-card> -->
         </v-flex>
       </v-layout>
     </v-container>
