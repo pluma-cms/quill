08ae470ab1d7d81baf4ec1dda58392d78fc4f14f
princessalto <princessellen0016@yahoo.com>
2018-09-18 22:27:54 +0800

    .

---
+++ src/components/ChartJS/LineChart.vue
@@ -0,0 +1,46 @@
+<template>
+  <canvas :id="name"></canvas>
+</template>
+
+<script>
+import { Chart } from 'chart.js/dist/Chart.bundle.js'
+
+export default {
+  name: 'LineChart',
+  props: {
+    name: {
+      type: [String],
+      default: 'lineChart',
+    },
+    data: {
+      type: [Object],
+      default: () => { return {} },
+    },
+    options: {
+      type: [Object],
+      default: () => { return {} },
+    },
+    ctx: {
+      type: [Object, Array, CanvasRenderingContext2D],
+      default: () => { return {} },
+    },
+  },
+
+  methods: {
+    initChart () {
+      /*eslint-disable*/
+      var ctx = document.getElementById(this.name).getContext('2d')
+      console.log(this.data, this.options)
+      var statisticsChart = new Chart(ctx, {
+        type: 'bar',
+        data: this.data,
+        options: this.options,
+      });
+    }
+  },
+
+  mounted () {
+    this.initChart()
+  }
+}
+</script>
--- src/modules/Pluma/Dashboard/Dashboard.vue
+++ src/modules/Pluma/Dashboard/Dashboard.vue
@@ -140,12 +140,14 @@
             </h4>
           </v-card-text>
           <v-card-text>
-            <!-- <line-chart
+            <line-chart
+              name="statisticsChart"
               :data="lineChart"
-              :options="lineChart.options"
+              :options="options"
+              :ctx="ctx"
               >
-            </line-chart> -->
-            <canvas id="statisticsChart"></canvas>
+            </line-chart>
+            <!-- <canvas id="statisticsChart"></canvas> -->
           </v-card-text>
         </v-card>
       </v-flex>
@@ -235,7 +237,7 @@
 </style>
 
 <script>
-import LineChart from '@/components/ChartJS/LineChart.js'
+import LineChart from '@/components/ChartJS/LineChart.vue'
 import store from '@/store'
 import { mapGetters } from 'vuex'
 import MyMostPopularCourses from '@/modules/Yggdrasil/Course/widgets/MyMostPopularCourses.vue'
@@ -265,107 +267,107 @@ export default {
   methods: {
     barChart () {
       /*eslint-disable*/
-      var ctx = document.getElementById('statisticsChart').getContext("2d");
+      this.ctx = document.getElementById('statisticsChart').getContext("2d");
+      console.log('dash.canvas', document.getElementById('statisticsChart').getContext('2d'))
 
-      var gradient = ctx.createLinearGradient(100, 0, 100, 400);
+      var gradient = this.ctx.createLinearGradient(100, 0, 100, 400);
       gradient.addColorStop(0, '#e56590');
       gradient.addColorStop(1, '#ffb88c');
 
-      var gradient2 = ctx.createLinearGradient(100, 0, 100, 400);
+      var gradient2 = this.ctx.createLinearGradient(100, 0, 100, 400);
       gradient2.addColorStop(0, '#4ccbab');
       gradient2.addColorStop(1, '#3e75c8');
 
-
-      var statisticsChart = new Chart(ctx, {
-          type: 'bar',
-          data: {
-            labels: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
-            datasets: [
-              {
-                label: "Statistics",
-                borderColor: gradient,
-                pointBorderColor: gradient,
-                pointBackgroundColor: gradient,
-                pointHoverBackgroundColor: gradient,
-                pointHoverBorderColor: gradient,
-                pointWidthRadius: 1,
-                pointBorderWidth: 5,
-                pointHoverRadius: 10,
-                pointHoverBorderWidth: 3,
-                borderWidth: 3,
-                pointRadius: 6,
-                fill: false,
-                backgroundColor: gradient,
-                data: [170, 80, 100, 120, 170, 120, -70, 140, 180, 100, -40, -70,]
-              },
-              {
-                label: "Statistics",
-                borderColor: gradient2,
-                pointBorderColor: gradient2,
-                pointBackgroundColor: gradient2,
-                pointHoverBackgroundColor: gradient2,
-                pointHoverBorderColor: gradient2,
-                pointWidthRadius: 1,
-                pointBorderWidth: 5,
-                pointHoverRadius: 10,
-                pointHoverBorderWidth: 3,
-                borderWidth: 3,
-                pointRadius: 6,
-                fill: false,
-                backgroundColor: gradient2,
-                reverse: true,
-                data: [-50, 40, 130, -20, -70, 50, 170, -40, -80, 100, 120, 150,]
-              },
-            ]
+      this.lineChart = {
+        labels: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+        datasets: [
+          {
+            label: "Statistics",
+            borderColor: gradient,
+            pointBorderColor: gradient,
+            pointBackgroundColor: gradient,
+            pointHoverBackgroundColor: gradient,
+            pointHoverBorderColor: gradient,
+            pointWidthRadius: 1,
+            pointBorderWidth: 5,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 3,
+            borderWidth: 3,
+            pointRadius: 6,
+            fill: false,
+            backgroundColor: gradient,
+            data: [170, 80, 100, 120, 170, 120, -70, 140, 180, 100, -40, -70,]
           },
-          options: {
-            animation: {
-              // easing: 'easeInBack'
-            },
-            layout: {
-              padding: {
-                top: 10,
-                bottom: 10,
-                left: 10,
-                right: 10,
-              }
-            },
-            legend: {
-              display: false,
-            },
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true,
-                  maxTicksLimit: 5,
-                  padding: 20,
-                  display: false
-                },
-                gridLines: {
-                  drawTicks: false,
-                  display: false
-                },
-              }],
-              xAxes: [{
-                barPercentage: 0.3,
-                gridLines: {
-                  zeroLineColor: "transparent",
-                  display: false
-                },
-                ticks: {
-                  padding: 20,
-                  display: false,
-                }
-              }]
-            }
+          {
+            label: "Statistics",
+            borderColor: gradient2,
+            pointBorderColor: gradient2,
+            pointBackgroundColor: gradient2,
+            pointHoverBackgroundColor: gradient2,
+            pointHoverBorderColor: gradient2,
+            pointWidthRadius: 1,
+            pointBorderWidth: 5,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 3,
+            borderWidth: 3,
+            pointRadius: 6,
+            fill: false,
+            backgroundColor: gradient2,
+            reverse: true,
+            data: [-50, 40, 130, -20, -70, 50, 170, -40, -80, 100, 120, 150,]
+          },
+        ]
+      }
+      this.options = {
+        animation: {
+          // easing: 'easeInBack'
+        },
+        layout: {
+          padding: {
+            top: 10,
+            bottom: 10,
+            left: 10,
+            right: 10,
           }
-      });
+        },
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              maxTicksLimit: 5,
+              padding: 20,
+              display: false
+            },
+            gridLines: {
+              drawTicks: false,
+              display: false
+            },
+          }],
+          xAxes: [{
+            barPercentage: 0.3,
+            gridLines: {
+              zeroLineColor: "transparent",
+              display: false
+            },
+            ticks: {
+              padding: 20,
+              display: false,
+            }
+          }]
+        }
+      }
     },
   },
 
   data () {
     return {
-      lineChart: {
+      lineChart: null,
+      options: null,
+      ctx: null,
+      linexxChart: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [
           {
