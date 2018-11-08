a5d9a6dac2fa450f3a9c51b1c2d61879a80f2f52
Princess <princessellen0016@yahoo.com>
2018-11-08 18:56:25 +0800

    .

--- src/modules/Pluma/Test/Show.vue
+++ src/modules/Pluma/Test/Show.vue
@@ -1,33 +1,27 @@
 <template>
   <section>
-    <!-- <v-container fluid grid-list-lg>
-      <v-layout row wrap justify-center align-center> -->
-        <!-- <v-flex md4 xs12>
-          <v-textarea
-            v-model="task"
-            label="Leave a comment"
-            solo
-            :href="create"
-            >
-          </v-textarea>
-
-          <v-btn text-xs-right @click.native.stop="create">
-            {{ trans('Submit') }}
-          </v-btn>
-
+    <v-container grid-list-lg>
+      <v-layout row wrap justify-center align-center>
+        <v-flex md10 xs12>
           <v-card flat class="grey lighten-4">
+            <strong>Tabs</strong> / Tab 1 (Course Title, Description) > Tab 2 (Meta) > Tab 3 (Playlist)
             <v-card-text>
-              <h4 class="grey--text">
+             <!--  <h4 class="grey--text">
                 Comments:&nbsp;
                 <v-fade-transition leave-absolute>
                   <span :key="`tasks-${tasks.length}`">
                     {{ tasks.length }}
                   </span>
                 </v-fade-transition>
-              </h4>
+              </h4> -->
+
+              Chapter 1
             </v-card-text>
           </v-card>
+
+
           <v-card
+            class="mb-3"
             v-if="tasks.length > 0"
             >
             <v-slide-y-transition
@@ -41,104 +35,90 @@
                   class="grey lighten-4"
                 ></v-divider>
 
-                <v-card-text :key="`${i}-${task.text}`">
-                  <v-layout>
-                    <v-avatar size="40" class="mr-3">
-                      <img src="https://images.unsplash.com/photo-1537730170185-27027b73b19c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7060dc523caa30db26a0fa1407328392&auto=format&fit=crop&w=400&q=60">
+                <v-card-text
+                  class="pa-4"
+                  :key="`${i}-${task.text}`"
+                  >
+                  <v-layout row align-center>
+                    <v-avatar size="60" class="mr-3">
+                      <img src="https://images.unsplash.com/photo-1541603617355-5f53b42522f9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1b7f9c25a0cce01cea8650d30a588629&auto=format&fit=crop&w=375&q=80" alt="">
                     </v-avatar>
-                    <v-card flat>
-                      <p class="mb-3">
-                        <strong class="mr-2">Princess Ellen Alto</strong>
-                        <span class="caption grey--text">Today at 5:42PM</span>
-                      </p>
-                      <p v-html="task.comment"></p>
-                    </v-card>
+                    <div>
+                      <p><strong v-html="task.title"></strong></p>
+                      <p v-html="task.body"></p>
+                    </div>
+                    <v-spacer></v-spacer>
+                    <v-tooltip bottom>
+                      <v-btn icon slot="activator">
+                        <v-icon small color="secondary">edit</v-icon>
+                      </v-btn>
+                      <span>{{ __('Edit') }}</span>
+                    </v-tooltip>
                   </v-layout>
                 </v-card-text>
+
+                <!-- edit-dialog -->
+                <v-edit-dialog
+                  :return-value.sync="task.name"
+                  large
+                  lazy
+                  persistent
+                  @save="save"
+                  @cancel="cancel"
+                  @open="open"
+                  @close="close"
+                >
+                  <div>{{ task.name }}</div>
+                  <div slot="input" class="mt-3 title">{{ __('Update') }}</div>
+                  <v-text-field
+                    slot="input"
+                    v-model="task.name"
+                    label="Edit"
+                    single-line
+                    counter
+                    autofocus
+                  ></v-text-field>
+                </v-edit-dialog>
               </template>
             </v-slide-y-transition>
           </v-card>
-        </v-flex> -->
+
+          <!-- editor -->
+          <form
+            action=""
+            @submit.prevent="create"
+            >
+            <v-text-field
+              v-model='title'
+              required
+              label="Part Title"
+              outline
+              >
+            </v-text-field>
+
+            <v-textarea
+              v-model='body'
+              label="Part Description"
+              outline
+              >
+            </v-textarea>
+
+            <v-btn type="submit" class="ma-0" outline>
+              {{ trans('Add') }}
+            </v-btn>
+          </form>
+
+          Chapter 2 Editor
+        </v-flex>
 
         <!-- <v-flex md8 xs12> -->
+          <!-- <v-card> -->
           <!-- <v-card> -->
             <!-- <v-card-text> -->
               <!-- <display-assignment></display-assignment> -->
             <!-- </v-card-text> -->
           <!-- </v-card> -->
         <!-- </v-flex> -->
-      <!-- </v-layout> -->
-    <!-- </v-container> -->
-
-    <v-container fluid grid-list-lg>
-      <v-layout row wrap justify-center align-center>
-        <v-flex md10 xs12>
-          <v-card>
-            <v-card-title class="emphasis--medium">
-              {{ __('Create Course') }}
-            </v-card-title>
-            <v-layout
-              justify-space-between
-              pa-3
-              >
-              <v-flex xs5>
-                <v-treeview
-                  :active.sync="active"
-                  :items="items"
-                  :load-children="fetchUsers"
-                  :open.sync="open"
-                  activatable
-                  active-class="primary--text"
-                  expand-icon="mdi-chevron-down"
-                  open-on-click
-                  transition
-                  >
-                  <v-icon
-                    v-if="!item.children"
-                    slot="prepend"
-                    class="title"
-                    slot-scope="{ item, active }"
-                    :color="active ? 'primary' : ''"
-                    >
-                    mdi-play-circle-outline
-                  </v-icon>
-                </v-treeview>
-              </v-flex>
-              <v-flex
-                d-flex
-                text-xs-center
-                >
-                <v-scroll-y-transition mode="out-in">
-                  <div
-                    v-if="!selected"
-                    class="grey--text text--lighten-1"
-                    style="align-self: center;"
-                  >
-                    {{ __('Select Part to Edit') }}
-                  </div>
-                  <v-card
-                    v-else
-                    :key="selected.id"
-                    class="pt-4 mx-auto"
-                    flat
-                    max-width="400"
-                    >
-                    <!-- <v-card-text>
-                      <h4 class="mb-2">
-                        {{ selected.name }}
-                      </h4>
-                      <div class="blue--text mb-2">{{ selected.email }}</div>
-                    </v-card-text> -->
-                    <v-card-text>
-                      <v-text-field outline label="Title"></v-text-field>
-                      <v-textarea label="Description" outline></v-textarea>
-                    </v-card-text>
-                  </v-card>
-                </v-scroll-y-transition>
-              </v-flex>
-            </v-layout>
-          </v-card>
-        </v-flex>
       </v-layout>
     </v-container>
   </section>
@@ -153,7 +133,7 @@ const pause = ms => new Promise(resolve => setTimeout(resolve, ms))
 export default {
   store,
 
-  name: 'TestShow',
+  name: 'Show',
 
   components: {
     DisplayAssignment
@@ -168,15 +148,13 @@ export default {
       tasks: [
         {
           done: false,
-          comment: 'Foobar'
-        },
-        {
-          fullname: 'Princess Ellen Alto',
-          timestamp: 'Yesterday',
-          comment: 'Fizzbuzz'
+          title: 'Foobar',
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro totam nisi nostrum ipsam? Inventore provident at accusamus aut, optio cum reprehenderit explicabo sed in. Odio rem enim asperiores voluptatem adipisci.'
         },
       ],
       task: null,
+      title: null,
+      body: null,
       resource: {
         rowsPerPageItems: [3, 6, 9, 12, 'All'],
         pagination: {
@@ -222,21 +200,33 @@ export default {
       this.tasks.push({
         done: false,
         comment: this.task,
+        title: this.title,
+        body: this.body,
       })
 
       this.task = null
+      this.title = null
+      this.body = null
     },
 
-    async fetchUsers (item) {
-      // Remove in 6 months and say
-      // you've made optimizations! :)
-      await pause(300)
-
-      return fetch('https://jsonplaceholder.typicode.com/comments/')
-        .then(res => res.json())
-        .then(json => (item.children.push(...json)))
-        .catch(err => console.warn(err))
+    save () {
+      this.snack = true
+      this.snackColor = 'success'
+      this.snackText = 'Data saved'
     },
+    cancel () {
+      this.snack = true
+      this.snackColor = 'error'
+      this.snackText = 'Canceled'
+    },
+    open () {
+      this.snack = true
+      this.snackColor = 'info'
+      this.snackText = 'Dialog opened'
+    },
+    close () {
+      console.log('Dialog closed')
+    }
   }
 }
 </script>
